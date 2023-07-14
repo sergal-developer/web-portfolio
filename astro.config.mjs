14c97fb3a0fa11f34632aebb3f13145f3c834be0
sergio.gallegos <sergio.gallegos@live.com.mx>
2023-07-13 21:22:21 -0600

    update astro aplication

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     outDir: './docs',
+    // root: './src',
+    // Resolves to the "./foo/public" directory in your current working directory
+    publicDir: '/web-portafolio/',
 });
